febb1df200e0edf6b88783b000c8e065e3b1fa71
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-09-16 11:25:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 import pandas as pd
-df = pd.read_excel(r'C:\Users\\17968\Desktop\works.xlsx')
+import random
+# 读取文件
+df = pd.read_excel(r'C:\Users\\17968\Desktop\work.xlsx')
+# 文件里的名字都是两个字的，需要进一步改善
 df['姓名'] = df['姓名'].apply(lambda x: x[-2:])
+# 检查文件的大概信息
+print(df.info)
+# 随机生成年龄
+age = [random.randint(18,70)for i in range(81)]
+# 检查年龄列表
+print(age)
+# 更新年龄列表
+df['年龄'] = age
+#检查文档文件
+print(df)
+# 保存文件
 df.to_excel(r'C:\Users\\17968\Desktop\work.xlsx',index=False)
