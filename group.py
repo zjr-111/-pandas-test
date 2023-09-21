dc63334f5d1990aaf38f19358c33e5a951ae37e1
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-09-21 18:09:21 +0800

    Update group.py

--- group.py
+++ group.py
@@ -4,3 +4,10 @@ df = pd.read_excel(r'C:\Users\zzz\Desktop\-pandas-test-main\work.xlsx')
 mean = df.groupby('学历')['薪资'].mean()
 # mean = df.groupby('学历')[['薪资', '年龄']].mean()
 print(mean)
+# 查看总体信息
+print(df.info())
+# 查看平均值、最大值、最小值那一类的信息
+print(df.describe())
+# 删除所有包含NaN的行
+# df.dropna()
+# 参数inplace=True直接在原表格删除 格式：df.describe(inplace=True)
