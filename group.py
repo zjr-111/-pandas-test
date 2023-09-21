4a70818d46b64f667c3b6e900537a402fa5e2e52
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-09-21 17:49:46 +0800

    Update group.py

--- group.py
+++ group.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df = pd.read_excel(r'C:\Users\zzz\Desktop\-pandas-test-main\work.xlsx')
+# 函数groupby()可以根据括号内的标准，进行别的列的分组，
 mean = df.groupby('学历')['薪资'].mean()
 # mean = df.groupby('学历')[['薪资', '年龄']].mean()
 print(mean)
