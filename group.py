3135e706c8ae067529cba7bf84d7d1b66ca6fe13
zjr-111 <127306838+zjr-111@users.noreply.github.com>
2023-09-22 18:10:24 +0800

    Update group.py

--- group.py
+++ group.py
@@ -1,13 +1,31 @@
 import pandas as pd
-df = pd.read_excel(r'work.xlsx')
-# 函数groupby()可以根据括号内的标准，进行别的列的分组，
-mean = df.groupby('学历')['薪资'].mean()
-# mean = df.groupby('学历')[['薪资', '年龄']].mean()
-print(mean)
-# 查看总体信息
-print(df.info())
-# 查看平均值、最大值、最小值那一类的信息
-print(df.describe())
-# 删除所有包含NaN的行
-# df.dropna()
-# 参数inplace=True直接在原表格删除 格式：df.describe(inplace=True)
+import matplotlib
+from collections import Counter
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+# 设置中文字体
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 设置中文字体为黑体
+plt.rcParams['axes.unicode_minus'] = False    # 解决负号显示问题
+
+df = pd.read_excel(r'C:\Users\zzz\Desktop\-pandas-test-main\work.xlsx')
+x_label = df['姓名'].reset_index(drop=True)
+y_label = df['薪资'].reset_index(drop=True)
+age = df['学历'].reset_index(drop=True)
+# 计数重复的次数
+count = Counter(age)
+# 将键为一个列表
+keys_list = list(count.keys())
+# 将值为一个列表
+values_list = list(count.values())
+# print(keys_list)
+# print(values_list)
+
+# 分片
+ax1 = plt.subplot(2, 2, 1)
+ax2 = plt.subplot(2, 2, 2)
+# 柱状图
+ax1.bar(x_label, y_label, width=0.3, color='red')
+# 饼图
+ax2.pie(values_list, labels=keys_list, autopct='%0.1f%%')
+plt.show()
